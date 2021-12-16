5bafec7de4c7274eb8d402a57c2635f07ecba1d7
Sun.Lei <sunlei012@outlook.com>
2021-12-16 14:02:36 +0800

    test antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "antd";
+import Footer from 'rc-footer';
+import 'rc-footer/assets/index.css'; // import 'rc-footer/asssets/index.less';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button type="primary">Button</Button>
+        <Button type="primary" className="-back-button">abc</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -19,6 +24,21 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer
+          columns={[
+            {
+              icon: (
+                  <img src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg" />
+              ),
+              title: '语雀',
+              items:[{
+                title:"ddd"
+              }]
+            },
+          ]}
+          bottom="Made with ❤️ by AFX"
+      />,
+      mountNode,
     </div>
   );
 }
